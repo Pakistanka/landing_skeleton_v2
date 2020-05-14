6d2e2cfcd6a5c4722f2708e5c71812ac89e7e68b
mac <mac@macs-MBP.fritz.box>
2020-05-14 22:41:46 +0200

    revert versions

--- js/animaiton.js
+++ js/animaiton.js
@@ -1,10 +1,7 @@
 let s_panel = document.querySelector(".s_panel");
 let s_panel_close_btn = document.querySelector(".s_panel_close_btn")
 let settings = document.querySelector(".settings");
-
 let priceBtn = document.querySelector(".price_btn");
-
-
 settings.onclick = openPanel = () => {
     gsap.to(".s_panel", {  visibility: 'visible'});
     gsap.to(".s_panel", {  duration: .5, autoAlpha: 1});
@@ -12,17 +9,11 @@ settings.onclick = openPanel = () => {
     gsap.to('.settings', { duration: .5, ease: "power4.out", autoAlpha: 0 })
 }
 priceBtn.onclick = openPanel;
-
 s_panel_close_btn.onclick = closePanel = () => {
     gsap.to(".s_panel", { duration: .5, autoAlpha: 0});
     gsap.to('.s_panel_close_btn', { autoAlpha: 0},"-=1");
     gsap.to('.settings', { duration: .5, ease: "power4.out", autoAlpha: 1 });
 }
-
-
-
-
-
 let base_opener = document.querySelector(".s_panel__base_label__show");
 base_open = true; 
 base_opener.onclick = toggleBase = () => {
@@ -41,8 +32,6 @@ base_opener.onclick = toggleBase = () => {
         base_open = true;
     }
 }
-
-
 let color_opener = document.querySelector(".s_panel__color_label__show");
 color_open = true; 
 color_opener.onclick = toggleBase = () => {
@@ -61,22 +50,16 @@ color_opener.onclick = toggleBase = () => {
         color_open = true;
     }
 }
-
-
 let blockAbout = document.querySelector(".s_panel__info_about"); 
 let blockFeedback = document.querySelector(".s_panel__info_wrap");
-
 let highLightAbout = false;
 let higtLightFeedback = false;
-
-
 blockAbout.onclick = toggleAbout = () => {
     if(highLightAbout===false){
         gsap.to('.s_panel__info_about', {duration: .2, background: '#fff'});
         gsap.to('.s_panel__info_wrap', {duration: .2, background: 'transparent'});
         gsap.to('.s_panel__info--block',{ visibility: "visible"});
         gsap.to('.s_panel__info--block',{durtation: .3, autoAlpha: 1});
-
         highLightAbout = true;
         highLightFeedback = false;
     } else if (highLightAbout){
@@ -86,7 +69,6 @@ blockAbout.onclick = toggleAbout = () => {
         highLightAbout = false;
     }
 }
-
 blockFeedback.onclick = toggleFeedback = () => {
     if (highLightFeedback === false){
         gsap.to('.s_panel__info_about', {duration: .2, background: 'transparent'});
@@ -96,7 +78,6 @@ blockFeedback.onclick = toggleFeedback = () => {
     
         highLightAbout = false;
         highLightFeedback = true;
-
     } else if (highLightFeedback){
         gsap.to('.s_panel__info_wrap', {duration: .2, background: 'transparent'});
         gsap.to('.s_panel__info--block',{durtation: .3, autoAlpha: 0});
@@ -104,32 +85,22 @@ blockFeedback.onclick = toggleFeedback = () => {
         highLightFeedback = false;
     }
 }
-
 gsap.from('.tooth', {opacity: 0, duration: 1, x: -20});
 gsap.from('.mushroom', {opacity: 0, duration: 1, x: -10});
 gsap.from('.eye', {opacity: 0, duration: 1, x: -40});
 gsap.from('.knife', {opacity: 0, duration: 1, x: -30});
 gsap.from('.herb', {opacity: 0, duration: 1, x: -10});
-
-
-
 gsap.from('.tongue', {opacity: 0, duration: 1, x: 30});
 gsap.from('.scissors', {opacity: 0, duration: 1, x: 40});
 gsap.from('.flower', {opacity: 0, duration: 1, x: 40});
 gsap.from('.bone', {opacity: 0, duration: 1, x: 20});
 gsap.from('.coffin', {opacity: 0, duration: 1, x: 40});
 gsap.from('.light', {opacity: 0, duration: 1, x: 40});
-
-
-
-
 let baseColor = document.querySelector(".base_bg_color");
 let basePattern = document.querySelector(".base_bg_pattern");
-
 baseColor.onclick = moveToPattern = () => {
     gsap.to('.underColorPattern', {duration: .3, x: 0});
 }
-
 basePattern.onclick = moveToColor = () => {
     gsap.to('.underColorPattern', {duration: .3, x: 115});
 }
@@ -145,10 +116,10 @@ if (w <= 1199){
         gsap.to(".s_panel", {  duration: .5, autoAlpha: 1});
         gsap.to('.s_panel_close_btn', { delay: .5, autoAlpha: 1 });
         gsap.to('.settings', { duration: .5, ease: "power4.out", autoAlpha: 0 })
-        
+
     }
     priceBtn.onclick = openPanel;
-    
+
     s_panel_close_btn.onclick = closePanel = () => {
         gsap.to(".s_panel", { duration: .5, autoAlpha: 0});
         gsap.to('.s_panel_close_btn', { autoAlpha: 0},"-=1");
@@ -167,4 +138,3 @@ if (w <= 767){
     // gsap.to('.settings', { duration: .5, ease: "power4.out", autoAlpha: 0 })
 
 }
-
